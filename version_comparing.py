f2d5aaf4e21f28518e9264dde6b92ada245b0f1c
repatik <dalalaler@gmail.com>
2019-10-13 17:40:53 +0300

    Added version comparing with using of module packaging

---
+++ version_comparing.py
@@ -0,0 +1,45 @@
+from packaging import version
+
+
+class Version:
+    def __init__(self, vers):
+        self.vers = version.parse(vers)
+        pass
+
+    def __eq__(self, other):
+        return self.vers == other
+
+    def __ne__(self, other):
+        return self.vers != other
+
+    def __lt__(self, other):
+        return self.vers < other
+
+    def __gt__(self, other):
+        return self.vers > other
+
+    def __le__(self, other):
+        return self.vers <= other
+
+    def __ge__(self, other):
+        return self.vers >= other
+
+
+def main():
+    to_test = [
+        ('1.0.0', '2.0.0'),
+        ('1.0.0', '1.42.0'),
+        ('1.2.0', '1.2.42'),
+        ('1.1.0-alpha', '1.2.0-alpha.1'),
+        ('1.0.1b', '1.0.10-alpha.beta'),
+        ('1.0.0-rc.1', '1.0.0'),
+    ]
+
+    for version_1, version_2 in to_test:
+        assert Version(version_1) < Version(version_2), 'le failed'
+        assert Version(version_2) > Version(version_1), 'ge failed'
+        assert Version(version_2) != Version(version_1), 'neq failed'
+
+
+if __name__ == "__main__":
+    main()
